5959813875e7bb52fc2d78fc2538823f833340d6
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2024-01-02 17:27:46 -0400

    feat: add websocket events

---
+++ src/services/asymmetric_key_service.ts
@@ -0,0 +1,11 @@
+export class AsymmetricKeyService {
+  async findPrivateKey(id: string) {
+    // TODO
+    return "";
+  }
+
+  async decrypt(privateKey: string, encryptedData: string) {
+    // TODO
+    return encryptedData;
+  }
+}
