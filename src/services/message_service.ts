0130ddb967cbf6cd755287bad380939222c6d087
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2024-01-02 12:46:38 -0400

    feat: add new use cases

---
+++ src/services/message_service.ts
@@ -0,0 +1,13 @@
+import { api } from "../api";
+import { Message } from "../entities/message";
+import { LOCAL_AUTH } from "./user_service";
+
+export class MessageService {
+  async fetchAll(friendId: string) {
+    const id = localStorage.getItem(LOCAL_AUTH);
+    const { data } = await api.get<Message[]>(
+      `/users/${id}/${friendId}/messages`
+    );
+    return data;
+  }
+}
--- src/services/user_service.ts
+++ src/services/user_service.ts
@@ -1,9 +1,76 @@
+import { api } from "../api";
+import { User, User2Create } from "../entities/user";
+
+const PATH = "/users";
+export const LOCAL_AUTH = "authid";
 export class UserService {
   logout() {
-    return;
+    localStorage.removeItem(LOCAL_AUTH);
   }
 
   hasAuth() {
-    return false;
+    const authid = localStorage.getItem(LOCAL_AUTH);
+    return !!authid;
+  }
+
+  async login(username: string, password: string) {
+    try {
+      const { data } = await api.post<User>(`${PATH}/login`, {
+        username,
+        password,
+      });
+      localStorage.setItem(LOCAL_AUTH, data.id);
+      return data;
+    } catch (error) {
+      return undefined;
+    }
+  }
+
+  async fetchAll() {
+    const { data } = await api.get<User[]>(PATH);
+    return data;
+  }
+
+  async findById(id: string) {
+    try {
+      const { data } = await api.get<User>(`${PATH}/${id}`);
+      return data;
+    } catch (error) {
+      return undefined;
+    }
+  }
+
+  async getAuthenticatedUser() {
+    try {
+      const id = localStorage.getItem(LOCAL_AUTH);
+      if (!id) throw new Error("Usuário sem as credenciais necessárias");
+      const { data } = await api.get<User>(`${PATH}/${id}`);
+      return data;
+    } catch (error) {
+      return undefined;
+    }
+  }
+
+  async create(newUser: User2Create) {
+    try {
+      const { data } = await api.post<User>(PATH, newUser);
+      return data;
+    } catch (error) {
+      return undefined;
+    }
+  }
+
+  async follow(user2Follow: string) {
+    try {
+      const id = localStorage.getItem(LOCAL_AUTH);
+      if (!id) throw new Error("Usuário sem as credenciais necessárias");
+      const { data } = await api.post<User>(
+        `/users/${id}/follow/${user2Follow}`
+      );
+      await api.post(`/users/${user2Follow}/follow/${id}`);
+      return data;
+    } catch (error) {
+      return undefined;
+    }
   }
 }
