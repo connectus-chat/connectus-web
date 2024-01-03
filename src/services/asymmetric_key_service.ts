a27fb448c8f7f803e2b0e3f4a639a5d3e15d3147
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2024-01-03 17:18:51 -0400

    feat: add find private key use function

--- src/services/asymmetric_key_service.ts
+++ src/services/asymmetric_key_service.ts
@@ -1,7 +1,15 @@
+import { api } from "../api";
+
 export class AsymmetricKeyService {
   async findPrivateKey(id: string) {
-    // TODO
-    return "";
+    try {
+      const { data } = await api.get<{ privateKey: string }>(
+        `/users/${id}/auth/private`
+      );
+      return data.privateKey;
+    } catch (error) {
+      return undefined;
+    }
   }
 
   async decrypt(privateKey: string, encryptedData: string) {
