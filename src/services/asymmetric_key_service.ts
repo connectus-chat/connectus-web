498f899e7dd226294b0a5ecfea75b00d8098ecdd
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2024-01-04 13:23:22 -0400

    feat: add key implementations

--- src/services/asymmetric_key_service.ts
+++ src/services/asymmetric_key_service.ts
@@ -12,8 +12,46 @@ export class AsymmetricKeyService {
     }
   }
 
+  private str2ab(str: string) {
+    const buf = new ArrayBuffer(str.length);
+    const bufView = new Uint8Array(buf);
+    for (let i = 0, strLen = str.length; i < strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+  }
+
+  private importPrivateKey(pem: string) {
+    const pemHeader = "-----BEGIN PRIVATE KEY-----";
+    const pemFooter = "-----END PRIVATE KEY-----";
+    const pemContents = pem.substring(
+      pemHeader.length,
+      pem.length - pemFooter.length
+    );
+    // base64 -> binary data
+    const binaryDerString = window.atob(pemContents);
+    // binary string -> ArrayBuffer
+    const binaryDer = this.str2ab(binaryDerString);
+
+    return window.crypto.subtle.importKey(
+      "pkcs8",
+      binaryDer,
+      {
+        name: "RSA-PSS",
+        hash: "SHA-256",
+      },
+      true,
+      ["sign"]
+    );
+  }
+
   async decrypt(privateKey: string, encryptedData: string) {
-    // TODO
-    return encryptedData;
+    const cryptKeyFromPrivateKey = await this.importPrivateKey(privateKey);
+    const decryptedDataWithPrivateKey = window.crypto.subtle.encrypt(
+      { name: "RSA-OAEP" },
+      cryptKeyFromPrivateKey,
+      new TextEncoder().encode(encryptedData)
+    );
+    return decryptedDataWithPrivateKey;
   }
 }
--- src/services/symmetric_key_service.ts
+++ src/services/symmetric_key_service.ts
@@ -1,11 +1,34 @@
+import * as Twofish from "twofish";
 export class SymmetricKeyService {
+  private str2ab(str: string) {
+    const buf = new ArrayBuffer(str.length);
+    const bufView = new Uint8Array(buf);
+    for (let i = 0, strLen = str.length; i < strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+  }
+
   async encrypt(key: string, message: string) {
-    // TODO
-    return message;
+    const twofish = new Twofish();
+    const dataArray = this.str2ab(message);
+    const keyArray = this.str2ab(key);
+    const cipherText = twofish.encrypt(keyArray, dataArray);
+    const encryptedString = cipherText
+      .map((x: number) => x.toString(16).padStart(2, "0"))
+      .join("");
+    return encryptedString;
   }
 
   async decrypt(key: string, encryptedMessage: string) {
-    // TODO
-    return encryptedMessage;
+    const twofish = new Twofish();
+    const encryptedMessageArray = this.str2ab(encryptedMessage);
+    const keyArray = this.str2ab(key);
+    const data = twofish.decrypt(keyArray, encryptedMessageArray);
+    const decryptedString = data
+      .map((x: number) => String.fromCharCode(x))
+      .join("");
+
+    return decryptedString;
   }
 }
